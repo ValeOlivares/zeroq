c956925cfa8b18a7acece406e668b6ecbd7d4cd7
ValeOlivares <valentinaolivares85@gmail.com>
2019-04-04 00:29:24 -0300

    create SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import logo from './images/logo.png'
+import logo from './images/logo.png';
+import { Container, Row, Col} from 'react-bootstrap';
+import SearchBar from './components/SearchBar'
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo}/>
-      </div>
+      <Container className='App'>
+        <Row>
+          <Col className='image-container'>
+            <img src={logo}/>
+          </Col>
+        </Row>
+        <SearchBar/>
+      </Container>
     );
   }
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,23 @@
+    
+import React, { Component } from 'react';
+import { Row, Col, Form} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <Row>
+        <Col className='searchbar-container'>
+          <Row>
+            <Col xs={11} sm={11} md={10} lg={4} xl={4}>
+              <Form.Control type="text" placeholder="Buscar sucursal" className= "searchbar"/>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export default SearchBar; 
