f1e062d23942b340d177348a6866b5eb18680d24
ValeOlivares <valentinaolivares85@gmail.com>
2019-04-03 23:08:07 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './images/logo.png'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
